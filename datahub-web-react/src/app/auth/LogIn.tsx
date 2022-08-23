4bcf7477a2df964b4e94dd5816608e13e1c3b7af
andrewp-nextphase <95711007+andrewp-nextphase@users.noreply.github.com>
2022-08-22 17:26:52 -0700

    Update LogIn.tsx

--- datahub-web-react/src/app/auth/LogIn.tsx
+++ datahub-web-react/src/app/auth/LogIn.tsx
@@ -1,23 +1,19 @@
 import React, { useCallback, useState } from 'react';
-import { Input, Button, Form, message, Image, Select } from 'antd';
+import * as QueryString from 'query-string';
+import { Input, Button, Form, message, Image } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { useReactiveVar } from '@apollo/client';
-import styled, { useTheme } from 'styled-components/macro';
-import { Redirect } from 'react-router';
+import styled, { useTheme } from 'styled-components';
+import { Redirect, useLocation } from 'react-router';
 import styles from './login.module.css';
 import { Message } from '../shared/Message';
 import { isLoggedInVar } from './checkAuthStatus';
 import analytics, { EventType } from '../analytics';
 import { useAppConfig } from '../useAppConfig';
-import { PageRoutes } from '../../conf/Global';
-import useGetInviteTokenFromUrlParams from './useGetInviteTokenFromUrlParams';
 
 type FormValues = {
-    fullName: string;
-    email: string;
+    username: string;
     password: string;
-    confirmPassword: string;
-    title: string;
 };
 
 const FormInput = styled(Input)`
@@ -42,43 +38,26 @@ const FormInput = styled(Input)`
     }
 `;
 
-const TitleSelector = styled(Select)`
-    .ant-select-selector {
-        color: white;
-        border: 1px solid #555555 !important;
-        background-color: transparent !important;
-    }
-    .ant-select-arrow {
-        color: white;
-    }
-`;
+export type LogInProps = Record<string, never>;
 
-export type SignUpProps = Record<string, never>;
-
-export const SignUp: React.VFC<SignUpProps> = () => {
+export const LogIn: React.VFC<LogInProps> = () => {
     const isLoggedIn = useReactiveVar(isLoggedInVar);
-    const inviteToken = useGetInviteTokenFromUrlParams();
+    const location = useLocation();
 
     const themeConfig = useTheme();
     const [loading, setLoading] = useState(false);
 
     const { refreshContext } = useAppConfig();
 
-    const handleSignUp = useCallback(
+    const handleLogin = useCallback(
         (values: FormValues) => {
             setLoading(true);
             const requestOptions = {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    fullName: values.fullName,
-                    email: values.email,
-                    password: values.password,
-                    title: values.title,
-                    inviteToken,
-                }),
+                body: JSON.stringify({ username: values.username, password: values.password }),
             };
-            fetch('/signUp', requestOptions)
+            fetch('/logIn', requestOptions)
                 .then(async (response) => {
                     if (!response.ok) {
                         const data = await response.json();
@@ -87,7 +66,7 @@ export const SignUp: React.VFC<SignUpProps> = () => {
                     }
                     isLoggedInVar(true);
                     refreshContext();
-                    analytics.event({ type: EventType.SignUpEvent, title: values.title });
+                    analytics.event({ type: EventType.LogInEvent });
                     return Promise.resolve();
                 })
                 .catch((error) => {
@@ -95,11 +74,13 @@ export const SignUp: React.VFC<SignUpProps> = () => {
                 })
                 .finally(() => setLoading(false));
         },
-        [refreshContext, inviteToken],
+        [refreshContext],
     );
 
-    if (isLoggedIn && !loading) {
-        return <Redirect to={`${PageRoutes.ROOT}`} />;
+    if (isLoggedIn) {
+        const params = QueryString.parse(location.search);
+        const maybeRedirectUri = params.redirect_uri;
+        return <Redirect to={(maybeRedirectUri && decodeURIComponent(maybeRedirectUri as string)) || '/'} />;
     }
 
     return (
@@ -109,85 +90,26 @@ export const SignUp: React.VFC<SignUpProps> = () => {
                     <Image wrapperClassName={styles.logo_image} src={themeConfig.assets?.logoUrl} preview={false} />
                 </div>
                 <div className={styles.login_form_box}>
-                    {loading && <Message type="loading" content="Signing up..." />}
-                    <Form onFinish={handleSignUp} layout="vertical">
+                    {loading && <Message type="loading" content="Logging in..." />}
+                    <Form onFinish={handleLogin} layout="vertical">
                         <Form.Item
-                            rules={[{ required: true, message: 'Please fill in your email!' }]}
-                            name="email"
+                            name="username"
                             // eslint-disable-next-line jsx-a11y/label-has-associated-control
-                            label={<label style={{ color: 'white' }}>Email</label>}
+                            label={<label style={{ color: 'white' }}>Username</label>}
                         >
-                            <FormInput prefix={<UserOutlined />} data-testid="email" />
+                            <FormInput prefix={<UserOutlined />} data-testid="username" />
                         </Form.Item>
                         <Form.Item
-                            rules={[{ required: true, message: 'Please fill in your name!' }]}
-                            name="fullName"
-                            // eslint-disable-next-line jsx-a11y/label-has-associated-control
-                            label={<label style={{ color: 'white' }}>Full Name</label>}
-                        >
-                            <FormInput prefix={<UserOutlined />} data-testid="name" />
-                        </Form.Item>
-                        <Form.Item
-                            rules={[
-                                { required: true, message: 'Please fill in your password!' },
-                                ({ getFieldValue }) => ({
-                                    validator() {
-                                        if (getFieldValue('password').length < 8) {
-                                            return Promise.reject(
-                                                new Error('Your password is less than 8 characters!'),
-                                            );
-                                        }
-                                        return Promise.resolve();
-                                    },
-                                }),
-                            ]}
                             name="password"
                             // eslint-disable-next-line jsx-a11y/label-has-associated-control
                             label={<label style={{ color: 'white' }}>Password</label>}
                         >
                             <FormInput prefix={<LockOutlined />} type="password" data-testid="password" />
                         </Form.Item>
-                        <Form.Item
-                            rules={[
-                                { required: true, message: 'Please confirm your password!' },
-                                ({ getFieldValue }) => ({
-                                    validator() {
-                                        if (getFieldValue('confirmPassword') !== getFieldValue('password')) {
-                                            return Promise.reject(new Error('Your passwords do not match!'));
-                                        }
-                                        return Promise.resolve();
-                                    },
-                                }),
-                            ]}
-                            name="confirmPassword"
-                            // eslint-disable-next-line jsx-a11y/label-has-associated-control
-                            label={<label style={{ color: 'white' }}>Confirm Password</label>}
-                        >
-                            <FormInput prefix={<LockOutlined />} type="password" data-testid="confirmPassword" />
-                        </Form.Item>
-                        <Form.Item
-                            rules={[{ required: true, message: 'Please fill in your title!' }]}
-                            name="title"
-                            // eslint-disable-next-line jsx-a11y/label-has-associated-control
-                            label={<label style={{ color: 'white' }}>Title</label>}
-                        >
-                            <TitleSelector placeholder="Title">
-                                <Select.Option value="Data Analyst">Data Analyst</Select.Option>
-                                <Select.Option value="Data Engineer">Data Engineer</Select.Option>
-                                <Select.Option value="Data Scientist">Data Scientist</Select.Option>
-                                <Select.Option value="Software Engineer">Software Engineer</Select.Option>
-                                <Select.Option value="Manager">Manager</Select.Option>
-                                <Select.Option value="Product Manager">Product Manager</Select.Option>
-                                <Select.Option value="Other">Other</Select.Option>
-                            </TitleSelector>
-                        </Form.Item>
                         <Form.Item style={{ marginBottom: '0px' }} shouldUpdate>
                             {({ getFieldsValue }) => {
-                                const { fullName, email, password, confirmPassword, title } = getFieldsValue();
-                                const fieldsAreNotEmpty =
-                                    !!fullName && !!email && !!password && !!confirmPassword && !!title;
-                                const passwordsMatch = password === confirmPassword;
-                                const formIsComplete = fieldsAreNotEmpty && passwordsMatch;
+                                const { username, password } = getFieldsValue();
+                                const formIsComplete = !!username && !!password;
                                 return (
                                     <Button
                                         type="primary"
@@ -196,7 +118,7 @@ export const SignUp: React.VFC<SignUpProps> = () => {
                                         className={styles.login_button}
                                         disabled={!formIsComplete}
                                     >
-                                        Sign Up!
+                                        Sign In
                                     </Button>
                                 );
                             }}

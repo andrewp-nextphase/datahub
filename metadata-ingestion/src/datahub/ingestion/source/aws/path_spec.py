bc16a6a8446b20ec2f489417c5f2b53806b95aaf
andrewp-nextphase <95711007+andrewp-nextphase@users.noreply.github.com>
2023-01-10 23:33:41 -0800

    Update path_spec.py

--- metadata-ingestion/src/datahub/ingestion/source/aws/path_spec.py
+++ metadata-ingestion/src/datahub/ingestion/source/aws/path_spec.py
@@ -16,7 +16,7 @@ from datahub.ingestion.source.aws.s3_util import is_s3_uri
 logging.getLogger("py4j").setLevel(logging.ERROR)
 logger: logging.Logger = logging.getLogger(__name__)
 
-SUPPORTED_FILE_TYPES: List[str] = ["csv", "tsv", "json", "parquet", "avro", "pdf"]
+SUPPORTED_FILE_TYPES: List[str] = ["csv", "tsv", "json", "parquet", "avro", "pdf", "sas7bdat", "xpt"]
 SUPPORTED_COMPRESSIONS: List[str] = ["gz", "bz2"]
 
 
